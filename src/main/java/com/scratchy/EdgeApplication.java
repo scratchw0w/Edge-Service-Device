6195cad2297e0058f55273843f8f7aa80871d817
Max Basov <maksym.basov@nixsolutions.com>
2021-04-14 18:49:28 +0300

    [edge-service] Added keycloak support.

--- src/main/java/com/scratchy/EdgeApplication.java
+++ src/main/java/com/scratchy/EdgeApplication.java
@@ -3,8 +3,14 @@ package com.scratchy;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableAsync
+@EnableCaching
 public class EdgeApplication {
 
     public static void main(String[] args) {
